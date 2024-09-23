765fbaabbddf099aa8268cf6eef328206a0fd70e
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-09-23 13:27:33 +0300

    add method test_delete_user,test_patch_in_data_user_without_job,test_patch_in_data_user_without_name,test_patch_in_data_user,test_put_in_data_user_without_job,test_put_in_data_user_without_name,test_put_in_data_user

--- core/contracts.py
+++ core/contracts.py
@@ -9,3 +9,22 @@ USER_DATA_SCHEME = {
     },
     "required": ["id", "email", "first_name", "last_name", "avatar"]
 }
+CREATE_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "id": {"type": "string"},
+        "createdAt": {"type": "string"}
+    },
+    "required": ["id"]
+}
+
+UPDATE_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "updatedAt": {"type": "string"}
+    }
+}
