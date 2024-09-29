8325c32528bd034d29be1ff265ad5953046f558c
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-09-29 22:26:23 +0300

    refactoring test_register_and_login_lesson.py

--- tests/test_register_and_login_lesson.py
+++ tests/test_register_and_login_lesson.py
@@ -11,6 +11,7 @@ REGISTER_USER = 'api/register'
 REGISTER_UNSUCCESSFUL_USER = 'api/register'
 LOGIN_USER = 'api/login'
 
+# Файл с данными пользователя для регистрации
 json_file = open('/Users/apple/PycharmProjects/MyApiTests/core/new_user_data.json')
 json_data = json.load(json_file)
 
@@ -20,27 +21,35 @@ def test_successful_user(json_data):
     assert response.status_code == 200
     validate(response.json(), REGISTERED_USER_SCHEME)
 
+# Файл с неудачными данными пользователя для регистрации
 json_file_with_invalid_data = open('/Users/apple/PycharmProjects/MyApiTests/core/invalid_user_data.json')
 json_data_register_user = json.load(json_file_with_invalid_data)
 
+# Тест на проверку неудачной регистрации:Неудачная, так как передается только логин: почта, пароль не передается
 @allure.suite('Проверки регистрации пользователя')
 @allure.title('Тест1:Проверка сценария на неаудачную регистрацию пользователя')
 @pytest.mark.parametrize('json_data_parametr', json_data_register_user)
 def test_register_user_unsuccessful(json_data_parametr):
+    #Обозначаем шаг в allure-отчете
     with allure.step(f'Делаем запрос по адресу: {BASE_URL + REGISTER_UNSUCCESSFUL_USER}'):
         try:
+            #Собираем URL и определяем тип запроса
             response = httpx.post(BASE_URL + REGISTER_UNSUCCESSFUL_USER, json=json_data_parametr)
         except httpx.HTTPStatusError as e:
+            #Выводим сообщение об ошибке,если запрос не прошел
             print(f"Ошибка HTTP: {e.response.status_code} - {e.response.text}")
-
+    # Обозначаем шаг в allure-отчете
     with allure.step('Проверяем код ответа 400'):
         try:
+            #Проверяем статус код ответа
             assert response.status_code == 400, f"Ожидали статус код 400, а получили {response.status_code}"
         except AssertionError as e:
             allure.attach(str(e), name="Ошибка проверки статуса", attachment_type=allure.attachment_type.TEXT)
 
+# Файл с данными пользователя для авторизации
 json_file_login_user = open('/Users/apple/PycharmProjects/MyApiTests/core/login_user.json')
 json_data_login_user = json.load(json_file_login_user)
+# Тест на проверку авторизации пользователя
 @allure.suite('Проверки авторизации пользователя')
 @allure.title("Тест2:Проверка сценария на удачную авторизацию пользователя")
 @pytest.mark.parametrize('json_data_parametr', [json_data_login_user])
@@ -60,7 +69,9 @@ def test_login_user(json_data_parametr):
     with allure.step('Проверяем,что ответ не пустой'):
         try:
             data = response.json()
-        # и тест завершится с сообщением "Ответ JSON пустой".
+            # Проверка data на пустоту
+            if not data:
+                raise ValueError("Переменная data пустая!")
         except ValueError:
             assert False, 'Ответ JSON пустой'
 
